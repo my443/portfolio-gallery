cd13c7fdd18abf7370b253df68393e0c0dba2fb6
John van Dijk <fortyfour-three@teksavvy.com>
2023-10-09 09:31:45 -0400

    Hover-over and popout functionality work.

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
-function pop_out_div(){
-    alert('hovered');
+function pop_out_div(elementID){
+    element = document.getElementById(elementID);
+    element = document.getElementById(elementID);
+    element.classList.add('center');
+
+    // cloned_element = element.cloneNode(true);
+    // cloned_element.id = elementID+'-expanded';
+    // cloned_element.classList.add('center');
+
+    // cloned_element.removeAttribute("onmouseover");
+    // document.getElementById('displayhere').appendChild(cloned_element);
+}
+
+function pop_down_div(elementID){
+    element = document.getElementById(elementID);
+    element.classList.remove('center');
 }
