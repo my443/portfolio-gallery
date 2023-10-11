30cc5eedb4e0ed7c2a18dd23dcf9c5c5abc914c4
John van Dijk <fortyfour-three@teksavvy.com>
2023-10-11 06:35:06 -0400

    Text from portfolio item moves into preview.

--- js.js
+++ js.js
@@ -15,3 +15,12 @@ function pop_down_div(elementID){
     element = document.getElementById(elementID);
     element.classList.remove('modal');
 }
+
+function fill_preview_area(sourceElementID){
+    headline = document.getElementById('item-1-headline').innerHTML;
+    tagline = document.getElementById('item-1-tagline').innerHTML;
+    
+    document.getElementById('headline').innerHTML = headline;
+    document.getElementById('tagline').innerHTML = tagline;
+    console.log(headline, tagline);
+}
